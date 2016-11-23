4dafc10390e57d17c9eb92df3456ff4531fa2faf
Thong Kuah <kuahyeow@gmail.com>
2016-11-23 21:03:36 +1300

    Fix a "instance variable @generations not initialized" warning

--- lib/partial_ks/runner.rb
+++ lib/partial_ks/runner.rb
@@ -55,7 +55,7 @@ class PartialKs::Runner
 
   protected
   def generations
-    return @generations if @generations
+    return @generations if defined?(@generations)
 
     @generations = []
     table_graphs.each do |table_graph|
