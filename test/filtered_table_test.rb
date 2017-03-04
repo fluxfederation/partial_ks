11f67d3c11c23f8d0bfa76226532054db4d92f5d
Thong Kuah <kuahyeow@gmail.com>
2017-03-04 21:41:04 +1300

    Allow lambdas to be used as a custom filter. This allows users to defer execution of their filters.
    Fixes #8

--- test/filtered_table_test.rb
+++ test/filtered_table_test.rb
@@ -21,6 +21,12 @@ describe "kitchen sync filter" do
     filtered_table.kitchen_sync_filter.must_equal({"only" => '"post_tags"."id" IN (1, 2)'})
   end
 
+  it "uses the filter inside a lambda" do
+    filter = -> { PostTag.where(:id => [1, 2]) }
+    filtered_table = PartialKs::FilteredTable.new(table, nil, custom_filter_relation: filter)
+    filtered_table.kitchen_sync_filter.must_equal({"only" => '"post_tags"."id" IN (1, 2)'})
+  end
+
   it "uses a SQL where fragment as a filter if provided" do
     string_filter = "1=0"
     filtered_table = PartialKs::FilteredTable.new(table, nil, custom_filter_relation: string_filter)
