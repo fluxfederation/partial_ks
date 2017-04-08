0c50d47f11e79d5617712b6ae8d1f4a39bcebd07
Thong Kuah <kuahyeow@gmail.com>
2017-04-08 23:02:22 +1200

    make private the models method on ConfigurationGenerator - saves the complexity in #all_tables

--- lib/partial_ks/configuration_generator.rb
+++ lib/partial_ks/configuration_generator.rb
@@ -3,7 +3,7 @@
 # and attempts to automatically populate the table into the table graph
 module PartialKs
   class ConfigurationGenerator
-    attr_reader :manual_configuration, :models
+    attr_reader :manual_configuration
 
     def initialize(manual_configuration, models: nil)
       @manual_configuration = manual_configuration
@@ -16,11 +16,12 @@ module PartialKs
 
     protected
     def all_tables
-      @all_tables ||= models.select{|model| tables_in_database.include?(model.table_name)}.map {|model| PartialKs::Table.new(model) }.select(&:model?).index_by(&:table_name)
+      @all_tables ||= models.map {|model| PartialKs::Table.new(model) }.select(&:model?).index_by(&:table_name)
     end
 
-    def tables_in_database
-      @tables_in_database ||= ActiveRecord::Base.connection.tables
+    def models
+      tables_in_database = ActiveRecord::Base.connection.tables
+      @models.select{|model| tables_in_database.include?(model.table_name)}
     end
 
     def filtered_tables
