93f7a7ddff851bee5caa5ced0488a115ef5c0926
Thong Kuah <kuahyeow@gmail.com>
2017-06-16 19:59:53 +1200

    Add more tests for all_rails_models

--- test/all_rails_models_test.rb
+++ test/all_rails_models_test.rb
@@ -16,4 +16,15 @@ describe "all rails tables" do
   it "returns the same number of models as the number of tables" do
     PartialKs.all_rails_models.map(&:table_name).sort.must_equal ActiveRecord::Base.connection.tables.sort
   end
+
+  it "does not choke on a table which has had its migration run" do
+    PartialKs.all_rails_models.wont_include NewModel
+  end
+
+  it "can return models with different table_name" do
+    model = OldEntry
+    model.table_name.wont_equal model.name.tableize
+
+    PartialKs.all_rails_models.must_include model
+  end
 end
