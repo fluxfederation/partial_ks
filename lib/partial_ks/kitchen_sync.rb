e035e23bef1d562aa91764815c595b48c9b8404f
Thong Kuah <kuahyeow@gmail.com>
2017-10-11 21:58:48 +1300

    Expose list of issues, via models_list

--- lib/partial_ks/kitchen_sync.rb
+++ lib/partial_ks/kitchen_sync.rb
@@ -1,13 +1,14 @@
 module PartialKs
   class KitchenSync
     attr_reader :models_list
+    delegate :issues, to: :models_list
 
     def initialize(manual_configuration)
-      @models_list = ModelsList.new(manual_configuration).all
+      @models_list = ModelsList.new(manual_configuration)
     end
 
     def run!(&block)
-      Runner.new(models_list).run!(&block)
+      Runner.new(models_list.all).run!(&block)
     end
   end
 end
